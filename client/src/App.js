51c76f128c71b463a99dc0a2f9bdfb25c140d439
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2021-08-21 18:59:54 +0530

    feat: react app initialized

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1>Stock Folio</h1>
+			<h1>Stock Folio</h1>
+			<h1>Stock Folio</h1>
+			<h1>Stock Folio</h1>
+			<h1>Stock Folio</h1>
+		</div>
+	);
+}
+
+export default App;
