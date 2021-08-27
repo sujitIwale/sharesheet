37a99d7e371d18c8d4ffb7fa6fa02e85e2392e89
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2021-08-27 23:59:44 +0530

    feat: server api update

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
+const liveSite = 'https://file-visualizer.netlify.app/';
 // fs.rmdirSync('./uploads/img', { recursive: true });
 
 app.use(cors());
 
 app.get('/', (req, res) => {
 	// const data = fs.readFileSync('rawdata/data.json', 'utf-8');
-	console.log('req');
-	res.send('Welcome to file-upload api');
+	res.send(`Welcome to file-upload api try to visit ${liveSite}`);
 });
 
 // Routes
