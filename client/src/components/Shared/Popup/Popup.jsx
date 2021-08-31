2123220089fec115deddf546deb89850edd959e6
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2021-08-31 19:47:10 +0530

    feat: alert icon and text added

--- client/src/components/Shared/Popup/Popup.jsx
+++ client/src/components/Shared/Popup/Popup.jsx
@@ -5,7 +5,13 @@ import './Popup.css';
 const PopupOverlay = ({ error }) => {
 	const content = (
 		<div className='popupBackground'>
-			<div className={`popupContainer ${error.type}`}>{error.msg}</div>
+			<div className={`popupContainer ${error.type}`}>
+				<i
+					className={`fas fa-${
+						error.type === 'danger' ? 'exclamation' : 'check-square'
+					}`}></i>
+				<h3 className='popup-text'>{error.msg}</h3>
+			</div>
 		</div>
 	);
 	return ReactDOM.createPortal(content, document.getElementById('popup'));
