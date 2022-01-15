8f46d32ef19627338bcaeef57a63428f12fa376e
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2022-01-15 09:52:03 +0530

    feat: client sign up added

--- validators/index.js
+++ validators/index.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 exports.runValidation = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res.status(422).json({
+		return res.status(203).json({
 			error: errors.array()[0].msg,
 		});
 	}
